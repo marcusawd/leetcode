0406ff585190a217a9b3637d88447d35a07e9894
marcusawd <angweidemarcus@gmail.com>
2024-02-27 15:58:12 +0800

    Extra Solution

--- Graphs/Binary Trees/DFS/E543.py
+++ Graphs/Binary Trees/DFS/E543.py
@@ -36,4 +36,32 @@ class Solution:
 
         return maxDia
 
+
+# You can also dynamically add variables .depth to the TreeNode class to store its depth there instead.
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        # Calculate from the bottom of the tree
+        stack = [(root, False)]
+        maxDia = 0
+
+        while stack:
+            node, visited = stack.pop()
+
+            if visited:
+                left = node.left.depth if node.left else 0
+                right = node.right.depth if node.right else 0
+                maxDia = max(maxDia, left + right)
+                node.depth = max(left, right) + 1
+
+            else:
+                stack.append((node, True))
+
+                if node.right:
+                    stack.append((node.right, False))
+                if node.left:
+                    stack.append((node.left, False))
+
+        return maxDia
+
+
 #! 35mins. Not easy!!
